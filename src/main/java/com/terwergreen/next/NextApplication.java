aec09aeb5a9831a5c6e9ab4d91bbd7ebf272c16c
Terwer <youweics@sina.com>
2019-01-10 16:52:33 +0800

    Create Project

---
+++ src/main/java/com/terwergreen/next/NextApplication.java
@@ -0,0 +1,14 @@
+package com.terwergreen.next;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextApplication.class, args);
+	}
+
+}
+
