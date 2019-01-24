33f7ac7c23c0df6a56367af5f1d8c952ec6ed53e
Terwer <youweics@sina.com>
2019-01-24 19:55:39 +0800

    Header is OK

--- src/main/webapp/src/components/themes/default/Header.vue
+++ src/main/webapp/src/components/themes/default/Header.vue
@@ -1,45 +1,87 @@
 <template>
   <b-container id="header">
     <b-row>
-      <b-col cols="*">
+      <b-col cols="3">
         <div class="imgWrap">
           <div class="align-middle">
-            <a href="/"><img class="logo" src="./images/logo.png" alt="Terwer"/></a>
+            <a href="/"
+              ><img class="logo" src="./images/logo.png" alt="Terwer"
+            /></a>
           </div>
         </div>
       </b-col>
-      <b-col cols="*" class="justify-content-center align-self-center">
-        <b-nav pills>
-          <b-nav-item active>Active</b-nav-item>
-          <b-nav-item>Link</b-nav-item>
+      <b-col cols="6" class="justify-content-center align-self-center">
+        <b-nav>
+          <b-nav-item to="/" active>首页</b-nav-item>
+          <b-nav-item>随笔</b-nav-item>
           <b-nav-item-dropdown
-            id="nav7_ddown"
-            text="Dropdown"
+            id="page"
+            text="页面"
             extra-toggle-classes="nav-link-custom"
-            right
+            variant="success"
+            offset="0"
           >
-            <b-dropdown-item>one</b-dropdown-item>
-            <b-dropdown-item>two</b-dropdown-item>
+            <b-dropdown-item>页面默认模板</b-dropdown-item>
+            <b-dropdown-item>页面模板01</b-dropdown-item>
+            <b-dropdown-item>页面模板02</b-dropdown-item>
             <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item>three</b-dropdown-item>
+            <b-dropdown-item>用户列表页</b-dropdown-item>
+            <b-dropdown-item>标签页面</b-dropdown-item>
           </b-nav-item-dropdown>
+          <b-nav-item>专题</b-nav-item>
+          <b-nav-item>图集</b-nav-item>
+          <b-nav-item>下载</b-nav-item>
+          <b-nav-item>视频</b-nav-item>
         </b-nav>
       </b-col>
+      <b-col cols="3" class="justify-content-center align-self-center">
+        <div class="header-right">
+          <div class="loginlink">
+            <a href="javascript:void(0);" @click="showModal"
+              ><i class="fa fa-search"></i><span>搜索</span></a
+            >
+            <a href="/auth/login"><i class="fa fa-user-circle"></i>登录</a>
+            <a href="/auth/register"
+              ><i class="fa fa-pencil-square-o"></i>注册</a
+            >
+          </div>
+        </div>
+      </b-col>
     </b-row>
+    <!--搜索遮罩层-->
+    <b-modal ref="searchModalRef" hide-footer title="全站搜索">
+      <!-- 搜索弹窗 -->
+      <div id="seacherModal" tabindex="-1">
+        <div>
+          <div class="next-seacher">
+            <div class="modal-body">
+              <b-form id="searchform" method="get" action="/">
+                <!-- Using components -->
+                <b-input-group prepend="">
+                  <b-form-input
+                    id="s"
+                    v-model="s"
+                    name="s"
+                    type="text"
+                    placeholder="输入关键词查找..."
+                  ></b-form-input>
+                  <b-input-group-append>
+                    <b-btn
+                      id="searchsubmit"
+                      type="submit"
+                      variant="primary"
+                      >搜索</b-btn
+                    >
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--<b-btn variant="danger" block @click="hideModal">关闭</b-btn>-->
+    </b-modal>
   </b-container>
-
-  <!--
-  <el-container id="header">
-    <el-aside class="logo">
-      <a href="/" title="terwer">
-        <img src="./images/logo.png" alt="terwer" />
-      </a>
-    </el-aside>
-    <el-main class="header-main">
-      menu
-    </el-main>
-  </el-container>
-  -->
 </template>
 
 <script>
@@ -54,9 +96,19 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      s: ""
+    };
   },
-  methods: {}
+  methods: {
+    showModal() {
+      this.$refs.searchModalRef.show();
+    }
+    // ,
+    // hideModal() {
+    //   this.$refs.searchModalRef.hide();
+    // }
+  }
 };
 </script>
 
