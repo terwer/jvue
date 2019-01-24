9be29e6e49370b82b4a8d534246a39bc83d85269
Terwer <csyouwei@gmail.com>
2019-01-24 23:31:23 +0800

    Edit menu

--- src/main/webapp/src/components/themes/default/Header.vue
+++ src/main/webapp/src/components/themes/default/Header.vue
@@ -26,12 +26,31 @@
             <b-dropdown-item>页面模板02</b-dropdown-item>
             <b-dropdown-divider></b-dropdown-divider>
             <b-dropdown-item>用户列表页</b-dropdown-item>
-            <b-dropdown-item>标签页面</b-dropdown-item>
+            <b-dropdown-item>标签页面2</b-dropdown-item>
           </b-nav-item-dropdown>
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                工具
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a class="dropdown-item" href="#">图集</a>
+                <a class="dropdown-item" href="#">下载</a>
+                <a class="dropdown-item" href="#">视频</a>
+              </div>
+            </li>
+          </ul>
           <b-nav-item>专题</b-nav-item>
-          <b-nav-item>图集</b-nav-item>
-          <b-nav-item>下载</b-nav-item>
-          <b-nav-item>视频</b-nav-item>
         </b-nav>
       </b-col>
       <b-col cols="3" class="justify-content-center align-self-center">
@@ -49,9 +68,14 @@
       </b-col>
     </b-row>
     <!--搜索遮罩层-->
-    <b-modal ref="searchModalRef" hide-footer title="全站搜索">
+    <b-modal
+      ref="searchModalRef"
+      v-model="modalShow"
+      hide-footer
+      title="全站搜索"
+    >
       <!-- 搜索弹窗 -->
-      <div id="seacherModal" tabindex="-1">
+      <div id="seacherModal">
         <div>
           <div class="next-seacher">
             <div class="modal-body">
@@ -62,14 +86,12 @@
                     id="s"
                     v-model="s"
                     name="s"
+                    ref="s"
                     type="text"
                     placeholder="输入关键词查找..."
                   ></b-form-input>
                   <b-input-group-append>
-                    <b-btn
-                      id="searchsubmit"
-                      type="submit"
-                      variant="primary"
+                    <b-btn id="searchsubmit" type="submit" variant="primary"
                       >搜索</b-btn
                     >
                   </b-input-group-append>
@@ -79,12 +101,12 @@
           </div>
         </div>
       </div>
-      <!--<b-btn variant="danger" block @click="hideModal">关闭</b-btn>-->
     </b-modal>
   </b-container>
 </template>
 
 <script>
+import Vue from "vue";
 export default {
   name: "Header",
   props: {
@@ -95,19 +117,28 @@ export default {
       }
     }
   },
+  watch: {
+    modalShow: function() {
+      if (this.modalShow) {
+        let self = this;
+        Vue.nextTick().then(function() {
+          self.$refs.s.focus();
+        });
+      }
+      // console.log("modalShow:" + this.modalShow);
+    }
+  },
   data() {
     return {
-      s: ""
+      s: "",
+      modalShow: false
     };
   },
   methods: {
     showModal() {
+      this.modalShow = !this.modalShow;
       this.$refs.searchModalRef.show();
     }
-    // ,
-    // hideModal() {
-    //   this.$refs.searchModalRef.hide();
-    // }
   }
 };
 </script>
@@ -130,5 +161,15 @@ export default {
 #header .imgWrap .logo {
   display: inline-block;
   max-height: 40px;
+}
+
+.nav li:hover > .dropdown-menu {
+  position: absolute;
+  display: block;
+}
+
+.dropdown-menu a:hover {
+  background: #007bff;
+  color: #fff;
 }
 </style>
