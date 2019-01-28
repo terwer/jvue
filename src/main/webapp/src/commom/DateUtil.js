80fbad4536ec4a151c4038fcb30d721b080587fd
Terwer <csyouwei@gmail.com>
2019-01-28 22:47:11 +0800

    Fit time

--- src/main/webapp/src/commom/DateUtil.js
+++ src/main/webapp/src/commom/DateUtil.js
@@ -1,3 +1,7 @@
+import { calendar } from "./Calendar";
+
+const lunar = calendar.solar2lunar();
+
 /**
  * 根据日期获取星期几
  * @returns {string} 星期
@@ -24,12 +28,13 @@ var getWeekByDay = function() {
 var getClientTime = function() {
   //获取客户端时间
   var now = new Date();
-  var currDatetime = now.getFullYear() + "年";
-  currDatetime +=
-    now.getMonth() + 1 > 9 ? now.getMonth() + 1 : "0" + (now.getMonth() + 1);
-  currDatetime += "月";
-  currDatetime += now.getDate() > 9 ? now.getDate() : "0" + now.getDate();
-  currDatetime += "日 ";
+  var currDatetime = "";
+  // currDatetime += now.getFullYear() + "年";
+  // currDatetime +=
+  //   now.getMonth() + 1 > 9 ? now.getMonth() + 1 : "0" + (now.getMonth() + 1);
+  // currDatetime += "月";
+  // currDatetime += now.getDate() > 9 ? now.getDate() : "0" + now.getDate();
+  // currDatetime += "日 ";
   currDatetime += now.getHours() > 9 ? now.getHours() : "0" + now.getHours();
   currDatetime += ":";
   currDatetime +=
@@ -40,8 +45,37 @@ var getClientTime = function() {
   return currDatetime;
 };
 
-var getTradTime = function() {
-  // 星期五 农历 腊月二十一
-  return getWeekByDay();
+var getPopTime = function() {
+  var popTime =
+    "阳历：" +
+    lunar.cYear +
+    "年" +
+    lunar.cMonth +
+    "月" +
+    lunar.cDay +
+    "日（" +
+    lunar.astro +
+    "）";
+  return popTime;
 };
-export { getClientTime, getTradTime };
+
+var getTradTime = function() {
+  var tradTime = "农历：" + lunar.lYear + "年" + lunar.IMonthCn + lunar.IDayCn;
+
+  return tradTime;
+};
+
+var getShengXiao = function() {
+  var shengxiao =
+    lunar.gzYear +
+    "年" +
+    lunar.gzMonth +
+    "月" +
+    lunar.gzDay +
+    "日（" +
+    lunar.Animal +
+    "年）";
+  return shengxiao;
+};
+
+export { getClientTime, getPopTime, getTradTime, getShengXiao, getWeekByDay };
--- src/main/webapp/src/components/themes/default/HeaderTime.vue
+++ src/main/webapp/src/components/themes/default/HeaderTime.vue
@@ -2,27 +2,46 @@
   <b-row id="headerTime">
     <b-col sm="0" md="0" lg="0" xl="2"></b-col>
     <b-col>
-      现在是
-      <span v-if="clientTime === ''"> 加载中... </span>
-      <span v-else> {{ clientTime }}</span>
-      <span class="next-line"><br /></span>
-      <span v-if="clientTime === ''"> 加载中... </span>
-      <span v-else>{{ tradTime }}</span>
-      天气晴
+      <b-row>
+        <b-col md="6" lg="6" xl="3" v-if="clientTime === ''"> 加载中... </b-col>
+        <b-col md="6" lg="6" xl="3" v-else
+          >现在是 {{ clientTime }} {{ weekday }}</b-col
+        >
+
+        <b-col md="6" lg="6" xl="3" v-if="popTime === ''"> 加载中... </b-col>
+        <b-col md="6" lg="6" xl="3" v-else class="item">{{ popTime }}</b-col>
+
+        <b-col md="6" lg="6" xl="3" v-if="tradTime === ''"> 加载中... </b-col>
+        <b-col md="6" lg="6" xl="3" v-else class="item">{{ tradTime }}</b-col>
+
+        <b-col md="6" lg="6" xl="3" v-if="shengxiao === ''"> 加载中... </b-col>
+        <b-col md="6" lg="6" xl="3" v-else class="item"
+          >生肖：{{ shengxiao }}</b-col
+        >
+      </b-row>
     </b-col>
     <b-col sm="0" md="2"></b-col>
   </b-row>
 </template>
 
 <script>
-import { getClientTime, getTradTime } from "../../../commom/DateUtil";
+import {
+  getClientTime,
+  getWeekByDay,
+  getPopTime,
+  getShengXiao,
+  getTradTime
+} from "../../../commom/DateUtil";
 export default {
   name: "HeaderTime",
   // 钩子函数
   mounted() {
     let that = this;
     that.clientTime = getClientTime();
+    that.weekday = getWeekByDay();
+    that.popTime = getPopTime();
     that.tradTime = getTradTime();
+    that.shengxiao = getShengXiao();
     setInterval(function() {
       that.clientTime = getClientTime();
     }, 2000);
@@ -30,38 +49,13 @@ export default {
   data() {
     return {
       clientTime: "",
-      tradTime: ""
+      weekday: "",
+      popTime: "",
+      tradTime: "",
+      shengxiao: ""
     };
   }
 };
 </script>
 
-<style scoped>
-/*
-##Device = iPad
-##Screen = B/w 768px - 1023px
-*/
-@media (min-width: 768px) and (max-width: 1023px) {
-  .next-line {
-    display: none;
-  }
-}
-/*
-##Device = iPad Pro
-##Screen = B/w 1024px - 1365px
-*/
-@media (min-width: 1024px) and (max-width: 1365px) {
-  .next-line {
-    display: none;
-  }
-}
-/*
-##Device = PC
-##Screen = B/w 1366px
-*/
-@media (min-width: 1366px) {
-  .next-line {
-    display: none;
-  }
-}
-</style>
+<style scoped></style>
