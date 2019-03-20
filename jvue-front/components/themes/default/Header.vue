c468357083d85fb100c6591008cfdd99cb3c1065
Terwer <youweics@sina.com>
2019-03-20 12:23:02 +0800

    Add search

--- jvue-front/components/themes/default/Header.vue
+++ jvue-front/components/themes/default/Header.vue
@@ -1,46 +1,81 @@
 <template>
   <el-row>
     <el-col :xs="24" :sm="24" :md="6">
-      <div class="grid-content bg-purple-green">&nbsp;</div>
+      <div class="img-wrap">
+        <nuxt-link to="/" class="float-left">
+          <div class="img-align-middle">
+            <img
+              class="logo"
+              src="../../../assets/logo.png"
+              alt="jvue"
+              title="jvue"
+            />
+          </div>
+        </nuxt-link>
+      </div>
     </el-col>
     <el-col :xs="24" :sm="24" :md="12">
-      <div class="grid-content bg-purple-light">Header</div>
+      <div class="grid-content bg-purple-light">HeaderMenu</div>
     </el-col>
     <el-col :xs="24" :sm="24" :md="6">
-      <div class="grid-content bg-purple-green">&nbsp;</div>
+      <div class="search">
+        <el-input v-model="s" placeholder="请输入关键字" clearable>
+          <el-button
+            slot="append"
+            type="primary"
+            icon="el-icon-search"
+            @click="doSearch"
+          >
+            搜索
+          </el-button>
+        </el-input>
+      </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
+import { getLogger } from "../../../util/logger";
+const logger = getLogger("components/themes/default/Header");
+
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      s: "",
+      searchLink: "/s/"
+    };
+  },
+  watch: {
+    s() {
+      this.searchLink = "/s/" + this.s;
+    }
+  },
+  methods: {
+    doSearch() {
+      logger.info("this.searchLink=>" + this.searchLink);
+      // this.$router.push({ path: this.searchLink });
+      // this.$router.go(this.searchLink);
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
+/**
+   * 图片居中
+   */
+.img-wrap {
+  background: #fff;
 }
-.el-col {
+.img-wrap .img-align-middle:before {
+  content: " ";
+  display: inline-block;
+  vertical-align: middle;
+  height: 100%;
 }
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-green {
-  height: auto;
-  background: #44f21e;
-}
-.grid-content {
-  min-height: 36px;
-}
-.row-bg {
-  background-color: #f9fafc;
+/* 搜索 */
+.search {
+  margin: 0 10px;
 }
 </style>
