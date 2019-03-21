2e079beadd1466d23a45fd2f77735f5769f8e6dc
Terwer <youweics@sina.com>
2019-03-21 12:00:30 +0800

    Fix axios param

---
+++ jvue-front/pages/post/_id.vue
@@ -0,0 +1,134 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-row>
+        <el-col :xs="0" :md="2">&nbsp;</el-col>
+        <el-col :xs="24" :md="20">
+          <el-main>
+            <el-container>
+              <el-main>
+                <el-container>
+                  <el-header>
+                    <HeaderTime />
+                  </el-header>
+                  <el-header>
+                    <Header />
+                  </el-header>
+                  <el-main>
+                    <div id="post">
+                      <!-- 导航 -->
+                      <el-breadcrumb separator="/">
+                        <el-breadcrumb-item
+                          v-for="item in items"
+                          :key="item.text"
+                          :to="item.to"
+                        >
+                          {{ item.text }}
+                        </el-breadcrumb-item>
+                      </el-breadcrumb>
+
+                      <!-- 文章标题 -->
+                      <div id="postTitle">
+                        <router-link
+                          :to="
+                            postObj.name === ''
+                              ? '/post/' + postObj.id + '.html'
+                              : '/post/' + postObj.name + '.html'
+                          "
+                        >
+                          <h1>
+                            {{ postObj.title }}
+                          </h1>
+                        </router-link>
+                      </div>
+                    </div>
+                  </el-main>
+                </el-container>
+              </el-main>
+            </el-container>
+          </el-main>
+        </el-col>
+        <el-col :xs="0" :md="2">&nbsp;</el-col>
+      </el-row>
+      <el-row>
+        <el-col>
+          <el-footer>
+            <Footer :site-config="siteConfigObj" />
+            <FriendLink />
+          </el-footer>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import { getLogger } from "../../util/logger";
+import HeaderTime from "../../components/themes/default/HeaderTime";
+import Header from "../../components/themes/default/Header";
+import Footer from "../../components/themes/default/Footer";
+import FriendLink from "../../components/themes/default/FriendLink";
+
+// 全局样式
+import "../../components/themes/default/style.css";
+const logger = getLogger("pages/post");
+
+export default {
+  components: { HeaderTime, Header, Footer, FriendLink },
+  data() {
+    return {
+      id: this.$route.params.id.replace(/\.[^/.]+$/, ""),
+      items: [
+        {
+          text: "首页",
+          to: { path: "/" }
+        },
+        {
+          text: "文章"
+        }
+      ]
+    };
+  },
+  async asyncData(context) {
+    const siteConfigResult = await context.$axios.$post("/site/config/list");
+    const siteConfigObj =
+      siteConfigResult.status === 1 ? siteConfigResult.data : {};
+
+    const id = context.route.params.id.replace(/\.[^/.]+$/, "");
+    const postParams = {
+      postSlug: id
+    };
+    const postResult = await context.$axios.$post(
+      "/blog/post/detail",
+      postParams
+    );
+    const postObj = postResult.status === 1 ? postResult.data : {};
+
+    logger.info("fetch siteConfig and post finish");
+
+    return { siteConfigObj, postObj };
+  },
+  head() {
+    return {
+      title: "文章" + " - " + this.siteConfigObj.webname,
+      meta: [
+        {
+          name: "keywords",
+          content: this.siteConfigObj.keywords
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: this.siteConfigObj.description
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+#post {
+  margin: 20px;
+}
+</style>
--- jvue-front/plugins/axios.js
+++ jvue-front/plugins/axios.js
@@ -5,22 +5,29 @@
  *@version 1.0
  *2019/3/19 16:39
  **/
+import qs from "qs";
 import { getLogger } from "../util/logger";
 const logger = getLogger("plugins/axios");
 
 export default function({ $axios, redirect }) {
   $axios.onRequest(config => {
-    logger.info("Making request to " + config.baseURL + config.url);
+    const url = config.baseURL + config.url;
+    logger.info("url=>" + url);
+
+    const params = config.data;
     logger.info("params=>");
-    console.log(config.data);
+    console.log(params || {});
+    config.data = qs.stringify(params);
   });
 
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status);
     if (code === 400) {
-      redirect("/400");
+      console.error("400");
+      // redirect("/400");
     } else if (code === 500) {
-      redirect("/sorry");
+      console.error("500");
+      // redirect("/sorry");
     }
   });
 }
