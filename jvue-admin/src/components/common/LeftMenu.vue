3eb6df3aafa84b3d89b541319ed7f343b44e8735
Terwer <youweics@sina.com>
2019-03-22 17:35:10 +0800

    Admin frame

---
+++ jvue-admin/src/components/common/LeftMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>LeftMenu</h1>
+</template>
+
+<script>
+export default {
+  name: "LeftMenu"
+};
+</script>
+
+<style scoped></style>
---
+++ jvue-admin/src/components/common/MHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>MHeader</h1>
+</template>
+
+<script>
+export default {
+  name: "MHeader"
+};
+</script>
+
+<style scoped></style>
