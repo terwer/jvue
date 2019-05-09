f7305f055e6c28215569f15ac01a6d18c400d71d
Terwer <youweics@sina.com>
2019-05-09 18:42:11 +0800

    rollback element-ui

--- jvue-front/plugins/element-ui.js
+++ jvue-front/plugins/element-ui.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import Element from "~/plugins/lib/element-ui";
-import locale from "~/plugins/lib/element-ui/lib/locale/lang/zh-CN";
+import Element from "~/element-ui";
+import locale from "~/element-ui/lib/locale/lang/zh-CN";
 import { getLogger } from "../util/logger";
-import "~/plugins/lib/element-ui/lib/theme-chalk/index.css";
+import "~/element-ui/lib/theme-chalk/index.css";
 const logger = getLogger("element-ui");
 
 export default () => {
