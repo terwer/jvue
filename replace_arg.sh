a22e8176905eeef1d988c64a6c25bc1c2fd47be0
terwer <youweics@163.com>
2022-06-19 22:33:30 +0800

    feat:Replace args

---
+++ replace_arg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+replaceArg() {
+  filename="application-rds.properties"
+  search=$1
+  replace=$2
+  if [[ $search != "" && $replace != "" ]]; then
+    sed -i "_bak" "s/$search/$replace/gi" $filename
+  fi
+}
+
+cd ./jvue-server/src/main/resources
+pwd
+
+replaceArg '${MYSQL_HOST}' "localhost"
+replaceArg '${MYSQL_PORT}' "3306"
+replaceArg '${MYSQL_DB_NAME}' "bugucms4"
+replaceArg '${MYSQL_USERNAME}' "terwer"
+replaceArg '${MYSQL_PASSWORD}' "123456"
+
+echo "done"
+
+
