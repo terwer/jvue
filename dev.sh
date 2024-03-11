b841862430296a13fadd9cc716997981e8e9df02
terwer <youweics@163.com>
2024-03-11 14:34:28 +0800

    fix: fix dev script

--- dev.sh
+++ dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# 终止占用8008端口的进程
-lsof -ti:8008 | xargs kill -9
+# 终止占用8008端口的进程，这里还要考虑必须是 java 进程
+# lsof -ti:8008 | xargs kill -9
 
-# 终止占用3000端口的进程
-lsof -ti:3000 | xargs kill -9
+# 终止占用3000端口的进程，这里还要考虑必须是 node 进程
+# lsof -ti:3000 | xargs kill -9
 
 # sudo n 18
 
@@ -14,3 +14,5 @@ cd jvue-server
 cd ../jvue-front
 ARTALK_SERVER_URL=https://v4.terwergreen.com:8003
 yarn dev
+
+# 需要考虑优雅退出
